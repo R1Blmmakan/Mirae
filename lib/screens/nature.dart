e6665fec6baad63c4692ba86290cf77531240aff
patur <grayanganzz@gmail.com>
2026-02-26 14:33:08 +0700

    banyak pokoknya

--- lib/screens/nature.dart
+++ lib/screens/nature.dart
@@ -7,49 +7,115 @@ class NaturePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xFFE5E9E9),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-            width: double.infinity,
-            padding: const EdgeInsets.only(top: 35, bottom: 30, left: 20),
-            decoration: const BoxDecoration(
-              color: Color(0xFFA3B18A),
-              borderRadius: BorderRadius.only(
-                bottomLeft: Radius.circular(45),
-                bottomRight: Radius.circular(45),
+      backgroundColor: const Color(0xFFA3B18A),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+
+            const SizedBox(height: 35),
+
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              child: Text(
+                "Nature",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 26,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
             ),
-            child: Row(
-              children: [
-                GestureDetector(
-                  onTap: () {
-                    if (kDebugMode) {
-                      print('Back button pressed');
-                    }
-                    Navigator.pop(context);
-                  },
-                  child: const Icon(
-                    Icons.arrow_back_ios,
-                    color: Colors.white,
-                    size: 20,
-                  ),
+
+            const SizedBox(height: 20),
+
+            Container(
+              width: double.infinity,
+              constraints: BoxConstraints(
+                minHeight: MediaQuery.of(context).size.height,
+              ),
+              padding: const EdgeInsets.symmetric(vertical: 10),
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.vertical(
+                  top: Radius.circular(40),
                 ),
-                const SizedBox(width: 10),
-                const Text(
-                  'Nature',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+            
+                  const SizedBox(height: 25),
+            
+                  
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    child: _buildLargeCard('assets/img/nature.webp.webp'),
                   ),
-                ),
-              ],
+            
+                  const SizedBox(height: 35),
+            
+                  const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 20),
+                    child: Text(
+                      "More Nature",
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+            
+                  const SizedBox(height: 15),
+            
+                  SizedBox(
+                    height: 120,
+                    child: ListView(
+                      scrollDirection: Axis.horizontal,
+                      padding: const EdgeInsets.symmetric(horizontal: 20),
+                      children: [
+                        _buildSmallCard('assets/img/mosque.webp'),
+                        _buildSmallCard('assets/img/burj_khalifa.webp'),
+                        _buildSmallCard('assets/img/airport.webp'),
+                        _buildSmallCard('assets/img/mosque.webp'),
+                        _buildSmallCard('assets/img/burj_khalifa.webp'),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
+
+static Widget _buildSmallCard(String image) {
+  return Container(
+    width: 120,
+    margin: const EdgeInsets.only(right: 15),
+    decoration: BoxDecoration(
+      borderRadius: BorderRadius.circular(15),
+      image: DecorationImage(
+        image: AssetImage(image),
+        fit: BoxFit.cover,
+      ),
+    ),
+  );
+}
+
+static Widget _buildLargeCard(String image) {
+  return Container(
+    width: double.infinity,
+    height: 335,
+    decoration: BoxDecoration(
+      borderRadius: BorderRadius.circular(50),
+      image: DecorationImage(
+        image: AssetImage(image),
+        fit: BoxFit.cover,
+      ),
+    ),
+  );
+}
 }
